e6848a7b8b591410a8e9f6587a81daca34b402dc
Azhou2023 <anz008@ucsd.edu>
2025-02-18 03:00:47 -0800

    incorporate images + extra data

--- api.py
+++ api.py
@@ -3,14 +3,13 @@ import sys
 import requests
 
 
-def calculate_affinity(ligand, protein):
+def get_docking_data(ligand, protein):
     response = requests.get(f"{os.environ["API_URL"]}?endpoint=run_docking&smiles={ligand}&target={protein}")
     if response.status_code==200:
-        affinity = response.json()
-        if "error" in affinity:
-            return 0
+        data = response.json()
+        if "error" in data:
+            return None
         else:
-            return affinity['binding_affinity']
+            return data
     else:
-        print("Error: ", response.status_code)
-        sys.exit()
+        return None
